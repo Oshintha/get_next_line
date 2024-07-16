cfa482fb829cf59ed73de9575f7f63a43cd2b9a3
Oshintha Sandaruwan <155742370+Oshintha@users.noreply.github.com>
2024-07-16 19:21:43 +0300

    Update testing.c

--- testing.c
+++ testing.c
@@ -1,23 +1,29 @@
 #include "get_next_line.h"
 
-
 #include <stdio.h>
 #include <fcntl.h>
 int	main(void)
 {
 	int		fd1;
-    char    buf[256];
-    int      chars_read;
+    char    buf[BUFFER_SIZE + 1];
+    ssize_t      chars_read;
 
 	//int		fd2;
 	//int		fd3;
+	
 	fd1 = open("tests/test1.txt", O_RDONLY);
+	chars_read = read(fd1, buf, BUFFER_SIZE);
 	//fd2 = open("tests/test2.txt", O_RDONLY);
 	//fd3 = open("tests/test3.txt", O_RDONLY);
-	while ((chars_read = read(fd1, buf, 100)))
+	while (chars_read > 0)
     {
         buf[chars_read] = '\0';
         printf("buf-> %s\n", buf);
+		chars_read = read(fd1, buf, BUFFER_SIZE);
+		if (chars_read == 0)
+			break ;
+		else if (chars_read == -1)
+		 return (-1);
     }
 
 	close(fd1);
